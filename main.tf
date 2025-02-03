f1b24d0482b2bd78b78dfc5edd2af9107bd56a39
sidartaoss <semprebono@gmail.com>
2025-02-02 22:54:02 -0300

    ci: add pipeline

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   monitoring             = false
   vpc_security_group_ids = ["sg-06f76a53f08a418d4"]
-  subnet_id              = "subnet-0bfb6070cab586136"
+  subnet_id              = "subnet-024323b388ab68f2c"
 
   tags = {
     Terraform   = "true"
@@ -17,7 +17,7 @@ module "ec2_instance" {
 
 terraform {
   backend "s3" {
-    bucket = "teste-repo-fullcycle-sidartaoss"
+    bucket = "teste-repo-fullcycle-sidartaoss"'
     key    = "teste"
     region = "sa-east-1"
   }
